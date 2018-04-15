4c6ea3229e09f5f57468d292c48aad795b1f57f7
Rocky Li <zl131@duke.edu>
2018-04-15 01:41:43 -0400

    Mobile support

--- buildsurvey.php
+++ buildsurvey.php
@@ -82,6 +82,7 @@ if(is_post_request()){
     <title>Build New Survey</title>
     <link rel="stylesheet" href="bootstrap.css">
     <link rel="shortcut icon" type="image/x-icon" href="hal.ico" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <style type="text/css">
